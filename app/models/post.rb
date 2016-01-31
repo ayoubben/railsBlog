fdbd4d8a177fe219990931e1cc1a03841313515f
bennani ayoub <ayoub.ben88@gmail.com>
2016-01-31 22:15:04 +0100

    Add image attachement in post s model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ActiveRecord::Base
-
+  has_attached_file :image, styles: { normal: "900x300>" }, default_url: "http://placehold.it/900x300"
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
   acts_as_taggable
 end
