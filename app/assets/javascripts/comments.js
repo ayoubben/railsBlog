f13715a64f91a6867c97936be458c1f5202b08f5
bennani ayoub <ayoub.ben88@gmail.com>
2016-02-01 21:52:50 +0100

    Fix authentification js and add user name in comments

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,8 +1,14 @@
 $(document).on('ready page:load', function() {
+  $("#authentification").hide();
   $("#comment-form").bind("ajax:complete", function(event,xhr,status){
     $('.text').val('');
     var url = window.location.href;
     $("#comments").load(url + " #comments");
   });
+
+  $( ".btn-login" ).click(function() {
+    $( "#authentification" ).toggle( 300 );
+    return false;
+   });
 
 });
