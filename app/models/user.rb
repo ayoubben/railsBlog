c93e84c6d8e44a73e113896a51b3dfbc2bdc2168
bennani ayoub <ayoub.ben88@gmail.com>
2016-02-01 23:31:28 +0100

    Add avatar to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_attached_file :avatar, styles: { mini: "64x64>" }, default_url: "http://placehold.it/64x64"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 end
