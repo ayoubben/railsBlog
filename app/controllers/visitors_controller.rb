a3333cdf29d049d7641656541814ec02ff946115
bennani ayoub <ayoub.ben88@gmail.com>
2016-02-05 21:55:52 +0100

    Add search by tags

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,5 +1,9 @@
 class VisitorsController < ApplicationController
   def index
-    @posts = Post.paginate(page: params[:page], per_page: 5)
+    if params[:tag]
+      @posts = Post.paginate(page: params[:page], per_page: 5).tagged_with(params[:tag])
+    else
+      @posts = Post.paginate(page: params[:page], per_page: 5)
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   mount Ckeditor::Engine => '/ckeditor'
   root to: 'visitors#index'
   devise_for :users
+  get 'tags/:tag', to: 'visitors#index', as: :tag
 
   resources :posts
   resources :comments
