9ddf6eec5c8f5ca9cfd6bf9e873130ed859387a0
bennani ayoub <ayoub.ben88@gmail.com>
2016-02-07 13:59:37 +0100

    Order post by created_at

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,7 +2,7 @@ class CategoriesController < ApplicationController
   before_action :set_post, only: [:show]
 
   def show
-    @posts = @category.posts.paginate(page: params[:page], per_page: 5)
+    @posts = @category.posts.paginate(page: params[:page], per_page: 5).order('created_at DESC')
     @item = @category.code
   end
 
--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,9 +1,9 @@
 class VisitorsController < ApplicationController
   def index
     if params[:tag]
-      @posts = Post.paginate(page: params[:page], per_page: 5).tagged_with(params[:tag])
+      @posts = Post.paginate(page: params[:page], per_page: 5).tagged_with(params[:tag]).order('created_at DESC')
     else
-      @posts = Post.paginate(page: params[:page], per_page: 5)
+      @posts = Post.paginate(page: params[:page], per_page: 5).order('created_at DESC')
     end
   end
 end
