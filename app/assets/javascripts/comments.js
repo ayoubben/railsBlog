78f46b4057d6c17f728abe6dd6bf783b56cfb209
bennani ayoub <ayoub.ben88@gmail.com>
2016-02-09 17:04:30 +0100

    Add and customise social medial tooltip

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,4 +1,5 @@
 $(document).on('ready page:load', function() {
+   $("a").tooltip();
   $("#comment-form").bind("ajax:complete", function(event,xhr,status){
     $('.text').val('');
     var url = window.location.href;
